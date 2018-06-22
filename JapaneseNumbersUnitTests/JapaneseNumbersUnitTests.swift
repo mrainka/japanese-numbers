1b86721a41ce1ca7cac64ec0d32aa07baf74c6ee
Marcin Rainka <marcin.rainka@gmail.com>
2018-06-22 22:22:15 +0200

    Add unit test target

---
+++ JapaneseNumbersUnitTests/JapaneseNumbersUnitTests.swift
@@ -0,0 +1,11 @@
+//
+//  JapaneseNumbersUnitTests.swift
+//  JapaneseNumbersUnitTests
+//
+//  Created by Marcin Rainka on 22/06/2018.
+//  Copyright © 2018 Marcin Rainka. All rights reserved.
+//
+
+import XCTest
+
+final class JapaneseNumbersUnitTests: XCTestCase {}
